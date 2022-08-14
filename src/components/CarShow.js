f424cbf07e74f765390f22325aae4ff533943f7f
Josh Bryson <josh@bryson.me.uk>
2022-08-14 16:12:24 +0100

    Ring display modified

--- src/components/CarShow.js
+++ src/components/CarShow.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { OrbitControls, PerspectiveCamera } from '@react-three/drei';
 import { Car } from './Car';
+import { Rings } from './Rings';
 
 export function CarShow() {
   return (
@@ -8,6 +9,7 @@ export function CarShow() {
       <OrbitControls target={[0, 0.35, 0]} maxPolarAngle={1.45} />
         <PerspectiveCamera makeDefault fov={50} position={[3, 2, 5]} />
         <color args={[0, 0, 0]} attach="background" />
+        <Rings />
         <Car />
         <spotLight
             angle={0.6}
---
+++ src/components/Rings.js
@@ -0,0 +1,41 @@
+import { useRef } from 'react';
+import { useFrame } from '@react-three/fiber';
+import { Color } from 'three';
+
+export function Rings() {
+    const itemsRef = useRef([]);
+
+    useFrame((state) => {
+        for (let i = 0; i < itemsRef.current.length; i++) {
+            let mesh = itemsRef.current[i];
+            let z = (i - 7) * 3.5;
+            mesh.position.set(0,0, -z);
+
+            let dist = Math.abs(z);
+            mesh.scale.set(1 - dist * 0.04, 1 - dist * 0.04, 1 - dist * 0.04);
+
+            if(i % 2 == 1) {
+                mesh.material.emissive = new Color(6, 0.15, 0.7).multiplyScalar(0.5);
+            } else {
+                mesh.material.emissive = new Color(0.1, 0.7, 3).multiplyScalar(0.5);
+            }
+        }
+    });
+
+    return (
+        <>
+            {[0,0,0,0,0,0,0,0,0,0,0,0,0,0].map((v, i) => (
+                <mesh
+                    castShadow
+                    key={i}
+                    receiveShadow
+                    position={[0,0,0]}
+                    ref={(el) => (itemsRef.current[i] = el)}
+                >
+                    <torusGeometry args={[3.35, 0.05, 16, 100]}/>
+                    <meshStandardMaterial color={[0,0,0]} emissive={[0.5, 0.5, 0.5]} />
+                </mesh>
+            ))}
+        </>
+    )
+}
