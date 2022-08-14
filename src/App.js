fce5297c94ca11c4c46f699b5529d364dbd9c194
Josh Bryson <josh@bryson.me.uk>
2022-08-14 19:07:29 +0100

    Postprocessing added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
+import { Bloom, ChromaticAberration, DepthOfField, EffectComposer } from '@react-three/postprocessing';
+import { BlendFunction } from 'postprocessing';
 import { CarShow } from './components/CarShow';
 import { Ground } from './components/Ground';
 import './style.css';
@@ -10,6 +12,27 @@ function App() {
       <Canvas shadows>
         <CarShow />
         <Ground />
+        <EffectComposer>
+          <DepthOfField 
+            bokehScale={3}
+            focusDistance={0.0035}
+            focalLength={0.01}
+            height={480}
+          />
+          <Bloom 
+            blendFunction={BlendFunction.ADD}
+            height={300}
+            intensity={1.3}
+            kernelSize={5}
+            luminanceSmoothing={0.025}
+            luminanceThreshold={0.15}
+            width={300}
+          />
+          <ChromaticAberration
+            blendFunction={BlendFunction.NORMAL}
+            offset={[0.0005, 0.0012]}
+          />
+        </EffectComposer>
       </Canvas>
     </Suspense>
   )
